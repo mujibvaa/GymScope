3cb435fa5af47f6b676cded3b6ea310919ace21f
mujibvaa <mujibvaaa@gmail.com>
2021-02-05 03:28:33 +0530

    Initial commit

---
+++ app/src/main/java/com/techiesatelier/gymscope/LauncherActivity.java
@@ -0,0 +1,34 @@
+package com.techiesatelier.gymscope;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    Button btlogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+
+        btlogin = findViewById(R.id.loginbt);
+
+
+        btlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LauncherActivity.this,LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
